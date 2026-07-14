c51537032d77ab56b24edce221cbff3111a8731a
Z User <z@container>
2026-07-14 18:30:31 +0000

    05633125-7f0b-4989-9e08-b9f62f8d320c

--- start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  NODE_OPTIONS="--max-old-space-size=4096" node .next/standalone/server.js -p 3000 >> /home/z/my-project/server.log 2>&1
-  echo "[$(date)] Server exited, restarting in 2s..." >> /home/z/my-project/server.log
-  sleep 2
-done
